aac9404856c0062ce34695be77ead90e344250ed
Aluno <Aluno@PC-43526>
2019-02-20 22:02:23 -0300

    Primeira aula

---
+++ src/main/java/com/github/braully/dws/controle/ClienteControle.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.braully.dws.controle;
+
+import com.github.braully.dws.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClienteControle {
+
+    Cliente cliente;
+
+    public ClienteControle() {
+        novoCliente();
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void novoCliente() {
+        this.cliente = new Cliente();
+    }
+
+    public void salvarCliente() {
+        String mensagem = " Cliente Salvo : " + cliente;
+        System.out.println(mensagem);
+    }
+}
---
+++ src/main/java/com/github/braully/dws/modelo/Cliente.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.braully.dws.modelo;
+
+import java.util.Date;
+
+
+public class Cliente {
+   
+    String nome;
+    String CPF;
+    Date dataNascimento;
+    String endereço;
+    String cidade;
+    String estado;
+    Boolean ativo;
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+    
+    
+}
