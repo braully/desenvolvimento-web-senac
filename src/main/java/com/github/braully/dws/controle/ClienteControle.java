54ec273b1a0a27ffd6c438e02e1776600bdf752d
strike <strike@alastor-note>
2019-02-20 21:54:56 -0300

    Primeira pagina JSF.

---
+++ src/main/java/com/github/braully/dws/controle/ClienteControle.java
@@ -0,0 +1,34 @@
+package com.github.braully.dws.controle;
+
+import com.github.braully.dws.modelo.Cliente;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClienteControle {
+
+    Cliente cliente;
+
+    public ClienteControle() {
+        novoCliente();
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public void novoCliente() {
+        this.cliente = new Cliente();
+    }
+
+    public void salvarCliente() {
+        String mensagem = "Cliente Salvo: " + cliente;
+        System.out.println(mensagem);
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(mensagem));
+    }
+}
---
+++ src/main/java/com/github/braully/dws/modelo/Cliente.java
@@ -0,0 +1,79 @@
+package com.github.braully.dws.modelo;
+
+import java.util.Date;
+
+public class Cliente {
+
+    String nome;
+    String cpf;
+    Date dataNascimento;
+    String endereco;
+    String cidade;
+    String estado;
+    Boolean ativo;
+
+    public Cliente() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nome=" + nome + ", cpf=" + cpf + ", dataNascimento=" + dataNascimento + ", endereco=" + endereco + ", cidade=" + cidade + ", estado=" + estado + ", ativo=" + ativo + '}';
+    }
+
+}
