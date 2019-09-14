ed1773bf1a6acd535c27601513c7568ba0e6de32
Braully Rocha <braully@gmail.com>
2019-09-13 21:43:25 -0300

    geral

--- src/main/java/com/github/braully/dws/UsuarioMB.java
+++ src/main/java/com/github/braully/dws/UsuarioMB.java
@@ -1,5 +1,7 @@
 package com.github.braully.dws;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -7,18 +9,20 @@ import org.springframework.stereotype.Component;
 @Scope("view")
 @Component
 public class UsuarioMB {
-
+    
     @Autowired
     UsuarioDAO usuarioDAO;
-
+    
     Usuario usuario = new Usuario();
-
+    
     public Usuario getUsuario() {
         return usuario;
     }
-
+    
     public void salvarUsuario() {
         usuarioDAO.save(usuario);
-        //usuario = new Usuario();
+        FacesContext.getCurrentInstance()
+                .addMessage(null, new FacesMessage("Usuário salvo com sucesso"));
+        usuario = new Usuario();
     }
 }
